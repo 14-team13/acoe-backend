fa7fc97462bebe97f5e62a5d5b28c572a633e46f
hyesooKim <hyesookim95@gmail.com>
2023-03-11 15:09:53 +0900

    chore: issue resolving test
    resolving #1

--- src/test/java/com/aluminium/acoe/AcoeApplicationTests.java
+++ src/test/java/com/aluminium/acoe/AcoeApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EcoeApplicationTests {
+class AcoeApplicationTests {
 
     @Test
     void contextLoads() {
