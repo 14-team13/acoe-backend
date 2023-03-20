52e32246b0dc78654ed94d4d127c0b71b5f7f3f1
Hanseunghoon <ok4343@hyundai-ite.com>
2023-03-20 18:46:14 +0900

    feat: User, Authority Entity 추가

---
+++ src/main/java/com/aluminium/acoe/entity/Authority.java
@@ -0,0 +1,18 @@
+package com.aluminium.acoe.entity;
+
+import lombok.*;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "authority")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Authority {
+
+    @Id
+    @Column(name = "authority_name", length = 50)
+    private String authorityName;
+}
