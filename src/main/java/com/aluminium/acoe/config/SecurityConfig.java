e3a1f520a04980abd9770dcdc94312d415e7411c
Hanseunghoon <ok4343@hyundai-ite.com>
2023-03-20 18:43:49 +0900

    feat:  401, 403 Unauthorized 해결을 위한 Security 설정 추가

---
+++ src/main/java/com/aluminium/acoe/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.aluminium.acoe.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@EnableMethodSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .requestMatchers("/api/authenticate", "/api/signup").permitAll()
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+}
