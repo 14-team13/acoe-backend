9545a116bfc03512546571b1efa746cb708ba7b6
Hanseunghoon <ok4343@hyundai-ite.com>
2023-03-28 20:36:37 +0900

    feat: swagger 설정 변경으로 인한 어노테이션 변경

--- src/main/java/com/aluminium/acoe/dto/UserDto.java
+++ src/main/java/com/aluminium/acoe/dto/UserDto.java
@@ -2,7 +2,7 @@ package com.aluminium.acoe.dto;
 
 import com.aluminium.acoe.entity.User;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -22,40 +22,21 @@ public class UserDto {
 
     @NotNull
     @Size(min = 3, max = 50)
-    @ApiModelProperty(
-            value = "ID",
-            name = "username",
-            dataType = "String",
-            example = "acoe"
-    )
+    @Schema(description = "사용자명")
     private String username;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @NotNull
     @Size(min = 3, max = 100)
-    @ApiModelProperty(
-            value = "PW",
-            name = "password",
-            dataType = "String"
-    )
+    @Schema(description = "비밀번호")
     private String password;
 
     @NotNull
     @Size(min = 3, max = 50)
-    @ApiModelProperty(
-            value = "별명",
-            name = "nickname",
-            dataType = "String",
-            example = "acoe"
-    )
+    @Schema(description = "닉네임")
     private String nickname;
 
-    @ApiModelProperty(
-            value = "권한",
-            name = "authorities",
-            dataType = "Set<AuthorityDto>",
-            example = "ROLE_USER, ROLE_ADMIN"
-    )
+    @Schema(description = "권한")
     private Set<AuthorityDto> authorities;
 
     public static UserDto from(User user) {
