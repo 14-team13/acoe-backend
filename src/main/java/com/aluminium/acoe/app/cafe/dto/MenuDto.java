87c1d22f9f2d965d26b67ff4d6855201e5a54a91
hyesooKim <hyesookim95@gmail.com>
2023-04-10 22:19:05 +0900

    feat: 카페 상세 정보 조회 테스트코드 추가

--- src/main/java/com/aluminium/acoe/app/cafe/dto/MenuDto.java
+++ src/main/java/com/aluminium/acoe/app/cafe/dto/MenuDto.java
@@ -1,6 +1,7 @@
 package com.aluminium.acoe.app.cafe.dto;
 
 import com.aluminium.acoe.common.dto.BaseDto;
+import lombok.Builder;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -9,6 +10,7 @@ import java.io.Serializable;
  * A DTO for the {@link com.aluminium.acoe.app.cafe.entity.Menu} entity
  */
 @Data
+@Builder
 public class MenuDto extends BaseDto {
     private Long menuId;
     private String menuNm;
--- src/test/java/com/aluminium/acoe/app/cafe/service/CafeServiceTest.java
+++ src/test/java/com/aluminium/acoe/app/cafe/service/CafeServiceTest.java
@@ -4,17 +4,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 
 import com.aluminium.acoe.app.cafe.dto.CafeDto;
+import com.aluminium.acoe.app.cafe.dto.MenuDto;
 import com.aluminium.acoe.app.cafe.entity.Cafe;
+import com.aluminium.acoe.app.cafe.entity.Menu;
 import com.aluminium.acoe.app.cafe.persistance.CafeRepository;
+import com.aluminium.acoe.app.cafe.persistance.MenuRepository;
 import com.aluminium.acoe.app.cafe.service.implementation.CafeServiceImpl;
 import com.aluminium.acoe.common.converter.CommonConverter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,30 +33,34 @@ class CafeServiceTest {
     private CafeRepository cafeRepository;
 
     @Mock
+    private MenuRepository menuRepository;
+
+    @Mock
     private CommonConverter commonConverter;
 
-    private static final Long areaCd = 3120000L;
-    private static final String cafeNm = "mockCafe";
+    private static final Long AREA_CD = 3120000L;
+    private static final String CAFE_NM = "mockCafe";
+    private static final String MENU_NM = "아메리카노";
 
 
     @Test
-    void searchList() {
+    void searchListTest() {
         // given
         Cafe cafe = com.aluminium.acoe.app.cafe.entity.Cafe.builder()
             .cafeId(11111L)
-            .cafeNm(cafeNm)
+            .cafeNm(CAFE_NM)
             .x(123L)
             .y(456L)
-            .areaCd(areaCd)
+            .areaCd(AREA_CD)
             .trdStateCd(1L)
             .build();
 
         CafeDto cafeDto = CafeDto.builder()
             .cafeId(11111L)
-            .cafeNm(cafeNm)
+            .cafeNm(CAFE_NM)
             .x(123L)
             .y(456L)
-            .areaCd(areaCd)
+            .areaCd(AREA_CD)
             .trdStateCd(1L)
             .build();
 
@@ -57,12 +68,64 @@ class CafeServiceTest {
         given(commonConverter.convertToGeneric(any(), any())).willReturn(cafeDto);
 
         // when
-        List<CafeDto> cafeDtos = cafeService.searchList(areaCd);
+        List<CafeDto> cafeDtos = cafeService.searchList(AREA_CD);
 
         // then
         cafeDtos.forEach(dto -> {
-            assertEquals(dto.getCafeNm(), cafeNm);
+            assertEquals(dto.getCafeNm(), CAFE_NM);
             assertEquals(dto.getTrdStateCd(), 1L);
         });
+    }
+
+    @Test
+    void getCafeTest() {
+        // given
+        Cafe cafe = Cafe.builder()
+                .cafeId(11111L)
+                .cafeNm(CAFE_NM)
+                .x(123L)
+                .y(456L)
+                .areaCd(AREA_CD)
+                .trdStateCd(1L)
+                .build();
+
+        Menu menu = Menu.builder()
+                .cafe(cafe)
+                .menuId(11111L)
+                .menuNm(MENU_NM)
+                .price(4300L)
+                .build();
+
+        MenuDto menuDto = MenuDto.builder()
+                .menuId(11111L)
+                .menuNm(MENU_NM)
+                .price(4300L)
+                .build();
+
+        CafeDto cafeDto = CafeDto.builder()
+                .cafeId(11111L)
+                .cafeNm(CAFE_NM)
+                .x(123L)
+                .y(456L)
+                .areaCd(AREA_CD)
+                .trdStateCd(1L)
+                .menuList(Arrays.asList(menuDto))
+                .build();
+
+        given(cafeRepository.findById(any())).willReturn(Optional.of(cafe));
+        given(commonConverter.convertToGeneric(any(), eq(CafeDto.class))).willReturn(cafeDto);
+        given(menuRepository.findByCafe_CafeId(any())).willReturn(Arrays.asList(menu));
+        given(commonConverter.convertToGeneric(any(), eq(MenuDto.class))).willReturn(menuDto);
+
+        // when
+        CafeDto resultDto = cafeService.getCafe(11111L);
+
+        // then
+        assertEquals(resultDto.getCafeNm(), CAFE_NM);
+        resultDto.getMenuList().forEach(dto-> {
+            assertEquals(dto.getMenuNm(), MENU_NM);
+            assertEquals(dto.getPrice(),4300L);
+        });
+
     }
 }
