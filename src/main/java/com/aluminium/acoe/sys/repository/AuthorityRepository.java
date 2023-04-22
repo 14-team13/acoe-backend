9228bec917c8fc73c2b96dd5cb8b881074dce7ad
Seunghoon Han <3246902@naver.com>
2023-04-22 14:07:35 +0900

    chore: 기존 로컬 로그인 소스 제거

--- src/main/java/com/aluminium/acoe/sys/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aluminium.acoe.sys.repository;
-
-import com.aluminium.acoe.sys.entity.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorityRepository extends JpaRepository<Authority, String> {
-}
