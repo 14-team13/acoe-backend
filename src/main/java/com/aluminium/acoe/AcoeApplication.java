02a18b112d2edbcb9e430207df63fa24ffa87c85
Hanseunghoon <ok4343@hyundai-ite.com>
2023-05-03 22:37:47 +0900

    feat: properties 어노테이션 수정

--- src/main/java/com/aluminium/acoe/AcoeApplication.java
+++ src/main/java/com/aluminium/acoe/AcoeApplication.java
@@ -1,9 +1,13 @@
 package com.aluminium.acoe;
 
+import com.aluminium.acoe.sys.config.properties.AppProperties;
+import com.aluminium.acoe.sys.config.properties.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(scanBasePackages = {"com.aluminium.acoe.sys.config.properties"})
+@SpringBootApplication
+@EnableConfigurationProperties({AppProperties.class, CorsProperties.class})
 public class AcoeApplication {
     public static void main(String[] args) {
         SpringApplication.run(AcoeApplication.class, args);
