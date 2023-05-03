c19cf55c2a075f7dcc237c42c878cf39d98fd765
Hanseunghoon <ok4343@hyundai-ite.com>
2023-05-04 00:49:30 +0900

    feat: oauth 코드 수정

--- src/main/java/com/aluminium/acoe/sys/oauth/info/OAuth2UserInfoFactory.java
+++ src/main/java/com/aluminium/acoe/sys/oauth/info/OAuth2UserInfoFactory.java
@@ -1,20 +1,17 @@
 package com.aluminium.acoe.sys.oauth.info;
 
-import com.aluminium.acoe.sys.oauth.type.ProviderType;
 import com.aluminium.acoe.sys.oauth.info.impl.GoogleOAuth2UserInfo;
 import com.aluminium.acoe.sys.oauth.info.impl.KakaoOAuth2UserInfo;
+import com.aluminium.acoe.sys.oauth.type.ProviderType;
 
 import java.util.Map;
 
 public class OAuth2UserInfoFactory {
     public static OAuth2UserInfo getOAuth2UserInfo(ProviderType providerType, Map<String, Object> attributes) {
-        switch (providerType) {
-            case GOOGLE:
-                return new GoogleOAuth2UserInfo(attributes);
-            case KAKAO:
-                return new KakaoOAuth2UserInfo(attributes);
-            default:
-                throw new IllegalArgumentException("유효하지 않음");
-        }
+        return switch (providerType) {
+            case GOOGLE -> new GoogleOAuth2UserInfo(attributes);
+            case KAKAO -> new KakaoOAuth2UserInfo(attributes);
+            default -> throw new IllegalArgumentException("유효하지 않음");
+        };
     }
 }
