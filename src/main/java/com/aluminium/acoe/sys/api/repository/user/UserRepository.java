d0664b09c318be5d7e3a0a57e642e0148febe9d9
hankyul20 <3246902@naver.com>
2023-05-13 15:27:47 +0900

    Feature/38 admin user management (#40)
    * feat: 관리자 유저 관리 기능 구현
    
    * feat: 관리자 유저 목록 페이징 처리
    
    * feat: 관리자 유저 목록 페이징 처리
    
    ---------
    
    Co-authored-by: Hanseunghoon <ok4343@hyundai-ite.com>

--- src/main/java/com/aluminium/acoe/sys/api/repository/user/UserRepository.java
+++ src/main/java/com/aluminium/acoe/sys/api/repository/user/UserRepository.java
@@ -1,6 +1,8 @@
 package com.aluminium.acoe.sys.api.repository.user;
 
 import com.aluminium.acoe.sys.api.entity.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,8 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUserId(String userId);
     void deleteUserByUserId(String userId);
+
+    Page<User> findByUserIdContaining(String userId, Pageable pageable);
+    Page<User> findByUsernameContaining(String username, Pageable pageable);
+    Page<User> findByEmailContaining(String email, Pageable pageable);
 }
